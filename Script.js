d2fe20fe93adf8dd82f40a33056a671ac7599e68
d0n4to <mecca4241@gmail.com>
2024-03-03 17:39:24 +0100

    update

--- Script.js
+++ Script.js
@@ -15,26 +15,3 @@ function checkTime(i) {
 }
 mostraOra();
 
-// Script.js
-
-document.addEventListener("DOMContentLoaded", function () {
-    // Sostituisci 'YOUR_API_KEY' con la tua chiave API di OpenWeatherMap
-    const apiKey = 'YOUR_API_KEY';
-    const city = 'London';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-    // Effettua la richiesta API al momento del caricamento della pagina
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Estrai le informazioni sulla temperatura
-            const temperature = data.main.temp;
-
-            // Aggiorna il paragrafo con l'id 'orario' nell'header
-            const orarioElement = document.getElementById('orario');
-            if (orarioElement) {
-                orarioElement.innerHTML = `Temperatura a ${city}: ${temperature}°C`;
-            }
-        })
-        .catch(error => console.error('Errore nella richiesta API meteo:', error));
-});
