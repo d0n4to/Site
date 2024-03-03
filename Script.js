e819b0955a1330127f2a79f42d8d836bf2091fce
d0n4to <mecca4241@gmail.com>
2024-03-03 20:50:26 +0100

    update

--- Script.js
+++ Script.js
@@ -15,3 +15,26 @@ function checkTime(i) {
 }
 mostraOra();
 
+document.addEventListener('DOMContentLoaded', function() {
+    var shareButton = document.getElementById('share-btn');
+    var url = 'https://ilribellepz.github.io/Site/';
+    var title = 'Titolo dell\'Ultimo post pubblicato: data (es.03 Mar)';
+    
+    shareButton.addEventListener('click', function() {
+        if (navigator.share) {
+            navigator.share({
+                title: title,
+                url: url
+            }).then(() => {
+                console.log('Articolo condiviso con successo');
+            }).catch((error) => {
+                console.error('Errore durante la condivisione:', error);
+            });
+        } else {
+            console.log('API navigator.share non supportata');
+            // Fallback se l'API non è supportata
+            // Puoi aprire manualmente la finestra di condivisione per i social media
+            // o utilizzare altre tecniche di condivisione compatibili con il browser
+        }
+    });
+});
